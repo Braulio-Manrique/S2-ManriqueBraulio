db213b426457ac4d696501add666969e4b5edffe
Braulio-Manrique <brauliocuenta12@gmail.com>
2024-11-10 09:57:59 -0500

    primer commit

---
+++ src/main/java/pe/com/braulio/S2_ManriqueBraulio/S2ManriqueBraulioApplication.java
@@ -0,0 +1,13 @@
+package pe.com.braulio.S2_ManriqueBraulio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2ManriqueBraulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2ManriqueBraulioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/braulio/S2_ManriqueBraulio/S2ManriqueBraulioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.braulio.S2_ManriqueBraulio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2ManriqueBraulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
